f4541092debd14f06f4358062e1643df844ba549
Nathan-Hutton <nathan.d.hutton@proton.me>
2025-07-21 20:12:15 -0500

    Can now load sun into 1planet1sun scene with json

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -47,6 +47,12 @@ GLint scene::pointLightCount {};
 void scene::readSceneJson(std::string filePath)
 {
 	std::ifstream file{ filePath };
+	if (!file.good())
+	{
+		std::cerr << "Json file path invalid: " << filePath << '\n';
+		std::exit(EXIT_FAILURE);
+	}
+
 	nlohmann::json sceneData;
 	file >> sceneData;
 
@@ -66,17 +72,202 @@ void scene::readSceneJson(std::string filePath)
 
 	camera::jsonSetup(sceneData);
 
-	for (const nlohmann::json& object : sceneData["objects"])
+	for (size_t i{ 0 }; i < sceneData["objects"].size(); ++i)
 	{
+		const nlohmann::json& object = sceneData["objects"][i];
+
 		if (!object.contains("type"))
 		{
-			std::cerr << "\nJson file is object is missing a type\n\n";
+			std::cerr << "\nJson file object " << i << " is missing a type\n\n";
 			std::exit(EXIT_FAILURE);
+		}
+
+		glm::vec3 position{ 0.0f };
+		if (object.contains("position") && object["position"].is_array() && object["position"].size() == 3)
+		{
+			position.x = object["position"][0];
+			position.y = object["position"][1];
+			position.z = object["position"][2];
+		}
+		else
+			std::cerr << "Object " << i << " is missing position info. Using defaults\n";
+
+		glm::vec3 velocity{ 0.0f };
+		if (object.contains("velocity") && object["velocity"].is_array() && object["velocity"].size() == 3)
+		{
+			velocity.x = object["velocity"][0];
+			velocity.y = object["velocity"][1];
+			velocity.z = object["velocity"][2];
+		}
+		else
+			std::cerr << "Object " << i << " is missing velocity info. Using defaults\n";
+
+		glm::vec3 rotationVector{ 0.0f };
+		float rotationSpeed{ 0.0f };
+		float angle{ 0.0f };
+		if (object.contains("rotation"))
+		{
+			nlohmann::json rotation = object["rotation"];
+
+			if (rotation.contains("rotationVector") && rotation["rotationVector"].is_array() && rotation["rotationVector"].size() == 3)
+			{
+				rotationVector.x = rotation["rotationVector"][0];
+				rotationVector.y = rotation["rotationVector"][1];
+				rotationVector.z = rotation["rotationVector"][2];
+			}
+			else
+				std::cerr << "Object " << i << " is missing rotation vector info. Using default\n";
+
+			if (rotation.contains("rotationSpeed"))
+				rotationSpeed = rotation["rotationSpeed"];
+			else
+				std::cerr << "Object " << i << " is missing rotation speed info. Using default\n";
+
+			if (rotation.contains("angle"))
+				angle = rotation["angle"];
+			else
+				std::cerr << "Object " << i << " is missing angle info. Using default\n";
+		}
+		else
+			std::cerr << "Object " << i << " rotation info missing. Using defaults\n";
+
+		float mass{ 1.0f };
+		if (object.contains("mass"))
+			mass = object["mass"];
+		else
+			std::cerr << "Object " << i << " mass info missing. Using defaults\n";
+
+		float radius{ 5.0f };
+		if (object.contains("radius"))
+			radius = object["radius"];
+		else
+			std::cerr << "Object " << i << " radius info missing. Using defaults\n";
+
+		int stacks{ 20 };
+		int slices{ 20 };
+		if (object.contains("resolution"))
+		{
+			if (object["resolution"].contains("stacks"))
+				stacks = object["resolution"]["stacks"];
+			else
+				std::cerr << "Object " << i << " stacks info missing. Using defaults\n";
+
+			if (object["resolution"].contains("slices"))
+				slices = object["resolution"]["slices"];
+			else
+				std::cerr << "Object " << i << " slices info missing. Using defaults\n";
+		}
+		else
+			std::cerr << "Object " << i << " resolution info missing. Using defaults\n";
+
+
+		std::shared_ptr<Texture> texture;
+		if (object.contains("texture"))
+		{
+			const std::string textureFilePath{ object["texture"] };
+			if (!resourceManager::textureExists(textureFilePath))
+				resourceManager::loadTextureIntoCache(textureFilePath);
+
+			texture = resourceManager::getTexture(textureFilePath);
+		}
+		else
+		{
+			std::cerr << "Object " << i << " texture info missing. Using default\n";
+			if (!resourceManager::textureExists("../assets/textures/plain.png"))
+				resourceManager::loadTextureIntoCache("../assets/textures/plain.png");
+
+			texture = resourceManager::getTexture("../assets/textures/plain.png");
 		}
 
 		if (object["type"] == "sun")
 		{
-			continue;
+			std::unique_ptr<Sun> sun { std::make_unique<Sun>(mass, radius, stacks, slices) };
+			sun->setPosition(position);
+			sun->setTexturePointer(texture);
+			sun->setRotation(rotationVector);
+			sun->setAngle(angle);
+			sun->setRotationSpeed(rotationSpeed);
+
+			int shadowWidth{ 1024 };
+			int shadowHeight{ 1024 };
+			float near{ 0.01f };
+			float far{ 100.0f };
+			float ambientIntensity{ 5.0f };
+			float diffuseIntensity{ 300.0f };
+			glm::vec3 attenuation{ 1.0f };
+			glm::vec3 color{ 1.0f };
+			if (object.contains("pointLight"))
+			{
+				const nlohmann::json& pointLight = object["pointLight"];
+
+				if (pointLight.contains("shadowWidth"))
+					shadowWidth = pointLight["shadowWidth"];
+				else
+					std::cerr << "Object " << i << " point light shadowWidth missing. Using default value\n";
+
+				if (pointLight.contains("shadowHeight"))
+					shadowHeight = pointLight["shadowHeight"];
+				else
+					std::cerr << "Object " << i << " point light shadowHeight missing. Using default value\n";
+
+				if (pointLight.contains("near"))
+					near = pointLight["near"];
+				else
+					std::cerr << "Object " << i << " point light near missing. Using default value\n";
+
+				if (pointLight.contains("far"))
+					far = pointLight["far"];
+				else
+					std::cerr << "Object " << i << " point light far missing. Using default value\n";
+
+				if (pointLight.contains("ambientIntensity"))
+					ambientIntensity = pointLight["ambientIntensity"];
+				else
+					std::cerr << "Object " << i << " point light ambientIntensity missing. Using default value\n";
+
+				if (pointLight.contains("diffuseIntensity"))
+					diffuseIntensity = pointLight["diffuseIntensity"];
+				else
+					std::cerr << "Object " << i << " point light diffuseIntensity missing. Using default value\n";
+
+				if (pointLight.contains("attenuation") && pointLight["attenuation"].is_array() && pointLight["attenuation"].size() == 3)
+				{
+					attenuation.x = pointLight["attenuation"][0];
+					attenuation.y = pointLight["attenuation"][1];
+					attenuation.z = pointLight["attenuation"][2];
+				}
+
+				else
+					std::cerr << "Object " << i << " point light attenuation missing. Using default value\n";
+
+				if (pointLight.contains("color") && pointLight["color"].is_array() && pointLight["color"].size() == 3)
+				{
+					color.x = pointLight["color"][0];
+					color.y = pointLight["color"][1];
+					color.z = pointLight["color"][2];
+				}
+				else
+					std::cerr << "Object " << i << " point light color missing. Using default value\n";
+			}
+			else
+				std::cerr << "Object " << i << " is a sun but contains no pointlight. Using defaults.\n";
+
+			sun->setPointLight(shadowWidth, shadowHeight, near, far, color.x, color.y, color.z, ambientIntensity, diffuseIntensity, attenuation[0], attenuation[1], attenuation[2]);
+			pointLights[pointLightCount++] = sun->getPointLight();
+
+			Sun* sunPtr{ sun.get() };
+			movables.push_back(std::move(sun));
+			stars.push_back(sunPtr);
+		}
+		else if (object["type"] == "planet")
+		{
+			if (object.contains("pointLight"))
+				std::cerr << "Object " << i << " is a planet but contains a pointlight\n";
+		}
+		else
+		{
+			std::cerr << "\nJson file object " << i << " type must be planet or sun\n\n";
+			std::exit(EXIT_FAILURE);
 		}
 	}
 }
@@ -97,21 +288,7 @@ void scene::createObjects1Sun1Planet()
 {
 	readSceneJson("../jsonScenes/1planet1sun.json");
 
-	resourceManager::loadTextureIntoCache("../assets/textures/sun.jpg");
-	resourceManager::loadTextureIntoCache("../assets/textures/earth.jpg");
 	resourceManager::loadMaterialIntoCache("../assets/materials/planetMaterial.json");
-
-    std::unique_ptr<Sun> sun { std::make_unique<Sun>(525.0f, 5.0f, 25, 25) };
-    sun->setPosition(glm::vec3{0.0f, 0.0f, -2.5f});
-    sun->setTexturePointer(resourceManager::getTexture("../assets/textures/sun.jpg"));
-    sun->setPointLight(1024, 1024, 0.01f, 100.0f, 1.0f, 1.0f, 1.0f, 5.0f, 300.0f, 0.1f, 0.1f, 0.01f);
-    sun->setRotation(glm::vec3{1.0f, 1.0f, 0.0f});
-    sun->setAngle(90.0f);
-    sun->setRotationSpeed(-25.0f);
-    pointLights[pointLightCount++] = sun->getPointLight();
-	Sun* sunPtr{ sun.get() };
-    movables.push_back(std::move(sun));
-	stars.push_back(sunPtr);
 
     std::unique_ptr<Planet> planet { std::make_unique<Planet>(4.0f, resourceManager::getMaterial("../assets/materials/planetMaterial.json"), 1.0f, 20, 20) };
     planet->setTexturePointer(resourceManager::getTexture("../assets/textures/earth.jpg"));
